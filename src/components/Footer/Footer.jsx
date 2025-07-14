d209b595a73d49e8cf4ca3bd1e850382a0a5d86a
Whizzyware <ubaniwisdom480@gmail.com>
2025-07-14 21:15:06 +0100

    added new feature

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,29 +6,89 @@ import instagram_icon from "../../assets/instagram_icon.png";
 import facebook_icon from "../../assets/facebook_icon.png";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className="footer">
-      <div className="footer-icons">
-        <img src={facebook_icon} alt="" />
-        <img src={instagram_icon} alt="" />
-        <img src={twitter_icon} alt="" />
-        <img src={youtube_icon} alt="" />
+    <footer className="netflix-footer">
+      <div className="footer-content">
+        <div className="social-links">
+          <a href="https://facebook.com">
+            <img src={facebook_icon} alt="Facebook" />
+          </a>
+          <a href="https://instagram.com">
+            <img src={instagram_icon} alt="Instagram" />
+          </a>
+          <a href="https://twitter.com">
+            <img src={twitter_icon} alt="Twitter" />
+          </a>
+          <a href="https://youtube.com">
+            <img src={youtube_icon} alt="Youtube" />
+          </a>
+        </div>
+        <div className="footer-links">
+          <div className="links-column">
+            <ul>
+              <li>
+                <a href="#">Audio Description</a>
+              </li>
+              <li>
+                <a href="#">Investor Relations</a>
+              </li>
+              <li>
+                <a href="#">Legal Notices</a>
+              </li>
+            </ul>
+          </div>
+          <div className="links-column">
+            <ul>
+              <li>
+                <a href="#">Help Center</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Cookie Preferences</a>
+              </li>
+            </ul>
+          </div>
+          <div className="links-column">
+            <ul>
+              <li>
+                <a href="#">Gift Cards</a>
+              </li>
+              <li>
+                <a href="#">Terms of Use</a>
+              </li>
+              <li>
+                <a href="#">Corporate Information</a>
+              </li>
+            </ul>
+          </div>
+          <div className="links-column">
+            <ul>
+              <li>
+                <a href="#">Media Center</a>
+              </li>
+              <li>
+                <a href="#">Privacy</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="service-code">
+          <button className="service-code-btn">Service Code</button>
+        </div>
+
+        <div className="copyright">
+          <p>© 1997-{currentYear} Netflix, Inc.</p>
+        </div>
       </div>
-      <ul>
-        <li>Audio Description</li>
-        <li>Help Center</li>
-        <li>Media Center</li>
-        <li>Investor Relations</li>
-        <li>Jobs</li>
-        <li>Term of Use</li>
-        <li>Privacy</li>
-        <li>Legal Notices</li>
-        <li>Cookie Prefrences</li>
-        <li>Corporate Information</li>
-        <li>Contact Us</li>
-      </ul>
-      <p className="copyright-text">© 1997-2025 Netflix, Inc. </p>
-    </div>
+    </footer>
   );
 };
 
